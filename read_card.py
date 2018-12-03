0c074c260c8819684564d50644478f8f3e43440e
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2018-12-03 09:54:21 +0100

    Updated code for card reader that now works with SSL. Updated error messages.

--- read_card.py
+++ read_card.py
@@ -1,11 +1,18 @@
 import sys
-from httplib2 import Http
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 
 def get_status_code(path):
-    h = Http()
-    resp, content = h.request(path, "GET")
-    return resp.status
+    try:
+        response = urlopen(path)
+        return response.getcode()
+    except HTTPError as e:
+        print("Arrrrrrrrrr! Noget gik galt. Find Kristoffer og sig ", e.code,
+              " til ham!")
+    except URLError as e:
+        print("Arrrrrrrrrr! Noget gik galt! Find Kristoffer og sig ", e.reason,
+              " til ham!")
 
 
 def get_card_number():
@@ -21,21 +28,20 @@ def get_card_number():
            38: '9',
            39: '0'}
 
-    print("Swipe your card: ")
+    print("Vis mig dit kort!")
     if sys.platform == "linux" or sys.platform == "linux2":
-        fp = open('/dev/hidraw0', 'rb')
-
-        ss = ""
-        done = False
-        while not done:
-            buffer = fp.read(11)
-            for c in buffer:
-                if c > 0:
-                    if c == 40:
-                        # 40 means the current card number is done
-                        done = True
-                        break
-                    ss += hid[c]
+        with open('/dev/hidraw0', 'rb') as fp:
+            ss = ""
+            done = False
+            while not done:
+                buffer = fp.read(11)
+                for c in buffer:
+                    if c > 0:
+                        if c == 40:
+                            # 40 means the current card number is done
+                            done = True
+                            break
+                        ss += hid[c]
     elif sys.platform == "win32":
         ss = str(input())
     else:
@@ -50,4 +56,4 @@ while 1:
     if(get_status_code(
                         "https://kort.codingpirates.dk/checkin/" + ss + "/"
                         ) == 200):
-        print("Velkommen")
+        print("Ahoy me hearties! Velkommen ombord på skibet! Arrrrrrrr!!!")
